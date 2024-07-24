b6f65dbb8fcdeaa8e69a978edcd99fb7df616148
birdkron <js8215001@gmail.com>
2024-07-23 18:06:25 -0600

    Create AutorController.cs

---
+++ Biblioteca.API/Controllers/AutorController.cs
@@ -0,0 +1,59 @@
+﻿using Biblioteca.BL.Interfaces;
+using Biblioteca.Entities.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Biblioteca.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        private readonly IAutorService service;
+
+        public AutorController(IAutorService service)
+        {
+            this.service = service;
+        }
+
+        // GET: api/<AutorController>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<AutorDTO>),(int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+
+        public async Task<IActionResult> GetAll()
+        {
+            IEnumerable<AutorDTO> result = (IEnumerable<AutorDTO>)await this.service.GetAutoresAsync();
+            return (result != null && result.Any()) ? (IActionResult)this.Ok(result) : (IActionResult)this.NoContent();
+        }
+
+        // GET api/<AutorController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(IEnumerable<AutorDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AutorController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AutorController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AutorController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
